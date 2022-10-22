217d699dee788bb0074168a870b4052efa069028
闲人 <code@idler8.com>
2022-10-22 14:12:13 +0800

    feat: 更新

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# 基础用法（localStorage）
 ```javascript
 import React from 'react';
 import useStorage from 'rh-storage'
@@ -14,5 +15,39 @@ function Layout(){
         <Userinfo />
         <Login />
     </>
+}
+```
+# 扩展用法（sessionStorage）
+```javascript
+import React from 'react';
+import { hookFactory, getWindowStorage } from 'rh-storage'
+const useStorage = hookFactory(getWindowStorage(window.sessionStorage));
+export default useStoragel
+```
+
+# 复杂用法（crypto）
+```javascript
+import AES from 'crypto-js/aes';
+import UTF8 from 'crypto-js/enc-utf8';
+const AESKey = 'www.idler8.com';
+export default function getWindowStorage(Storage){
+    return {
+        getItem(key){
+            try {
+                return AES.decrypt(Storage.getItem(key), AESKey).toString(UTF8);
+            } catch (e) {
+                return null;
+            }
+        },
+        setItem(key, value){
+            Storage.setItem(key,AES.encrypt(value, AESKey).toString())
+        },
+        removeItem(key){
+            Storage.removeItem(key)
+        },
+        clear(){
+            Storage.clear();
+        }
+    }
 }
 ```
--- index.js
+++ index.js
@@ -1,37 +1,51 @@
 import { useCallback, useEffect, useState } from 'react';
-// TODO 多页面监听
-export function factory(getItem, setItem) {
+export function addWindowListener(Storage){
+  const handler = ({key,oldValue,newValue})=>{
+    if(key === undefined) return Storage.clear();
+    if(!newValue === undefined) return Storage.remove(key);
+    if(!oldValue === undefined) return Storage.create(key,newValue);
+    return Storage.update(key,newValue);
+  }
+  window.addEventListener('storage',handler)
+  return ()=>window.removeEventListener(handler)
+}
+export function hookFactory(Storage){
   const Cache = {};
   const Listener = [];
-  function setStorage(key, value, ignoreEvents) {
-    setItem(key, value);
-    Cache[key] = value;
-    if (!ignoreEvents) Listener.forEach((fn) => fn(key));
-  }
-  function getStorage(key) {
-    return Cache[key] || getItem(key);
-  }
-  function useStorage(key) {
-    const [ value, setValue ] = useState(() => getStorage(key));
-    const updateValue = useCallback((value) => setStorage(key, value), [ key ]);
+  return function useStorage(key) {
+    const [ value, setValue ] = useState(() => Storage.get(key));
+    const updateValue = useCallback((value) => Storage.update(key, value), [ key ]);
     useEffect(() => {
-      setValue(getStorage(key));
-      const listenFunc = (keyChange) => keyChange === key && setValue(Cache[key]);
-      Listener.push(listenFunc);
-      return () => Listener.splice(Listener.indexOf(listenFunc), 1);
+      setValue(Storage.get(key));
+      const handler = (keyChange) => keyChange === key && setValue(Cache[key]);
+      Listener.push(handler);
+      return () => Listener.splice(Listener.indexOf(handler), 1);
     }, [ key ]);
     return [ value, updateValue ];
   }
-  useStorage.setStorage = setStorage;
-  useStorage.getStorage = getStorage;
-  return useStorage;
 }
-export default factory(function(key, value) {
-  localStorage[key] = JSON.stringify(value);
-}, function(key) {
-  try {
-    return JSON.parse(localStorage[key]);
-  } catch (e) {
-    return null;
+export function getWindowStorage(Storage){
+  return {
+    get(key){
+      try {
+        return JSON.parse(Storage.getItem(key));
+      } catch (e) {
+        return null;
+      }
+    },
+    update(key,value){
+      Storage.setItem(key, JSON.stringify(value))
+    },
+    remove(key){
+      Storage.removeItem(key);
+    },
+    clear(){
+      Storage.clear();
+    },
+    create(key, value){
+      this.update(key,value)
+    }
   }
-});
+}
+const useStorage = hookFactory(getWindowStorage(localStorage))
+export default useStorage;
